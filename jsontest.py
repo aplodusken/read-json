96e30920ff5b1d24335a56101ac29296c7239efb
Micke <micke@aplodusken.se>
2022-11-11 08:29:43 +0100

    jsontest is messed up

--- jsontest.py
+++ jsontest.py
@@ -7,24 +7,27 @@ f = open('test.json',)
 # returns JSON object as
 # a dictionary
 data = json.load(f)
-  
-#print(data['segments'][1]['words'][1]['text']) 
-row = 0
-rows = len(data['segments'])
-print()
+
+print(data['segments'])
+for row in data['segments'][0]['words']
+    print(row['words'][0])
+    #print(data['segments'][1]['words'][1]['text']) 
+    #row = 0
+    #rows = len(data['segments'])
+    #print()
 # Iterating through the json
 # list
-row_string = data['segments'][row]['speaker']
-row_string += "\t"
-row_string += str(data['segments'][row]['words'][0]['start'])
-row_string += "\t"
-row_string += "white \t"
+    ##row_string = row['speaker']
+    ##row_string += "\t"
+    ##row_string += str(row['words'][0]['start'])
+    ##row_string += "\t"
+    ##row_string += "white \t"
 
-for i in data['segments'][1]['words']:
-    row_string += i['text']
-    row_string += " "
+    ##for i in row['words']:
+     ##   row_string += i['text']
+     ##   row_string += " "
     
-print(row_string)
+    ##print(row_string)
   
 # Closing file
 f.close()
