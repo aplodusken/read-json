db866ef422640f0138a72422338676bb99ebbf7e
Micke <micke@aplodusken.se>
2022-11-23 21:20:55 +0100

    WORKING populate a dictionary with a list of speak

--- jsontest.py
+++ jsontest.py
@@ -1,33 +1,27 @@
 import json
-
-  
-# Opening JSON file
-f = open('test.json',)
-  
 # returns JSON object as
 # a dictionary
+f = open('test.json',)
 data = json.load(f)
 
-print(data['segments'])
-for row in data['segments'][0]['words']
-    print(row['words'][0])
-    #print(data['segments'][1]['words'][1]['text']) 
-    #row = 0
-    #rows = len(data['segments'])
-    #print()
-# Iterating through the json
-# list
-    ##row_string = row['speaker']
-    ##row_string += "\t"
-    ##row_string += str(row['words'][0]['start'])
-    ##row_string += "\t"
-    ##row_string += "white \t"
+#for speakers in data['speakers']:
+#    print(speakers['name'])
+#    print(speakers['spkid'])
 
-    ##for i in row['words']:
-     ##   row_string += i['text']
-     ##   row_string += " "
-    
-    ##print(row_string)
+#print(data['speakers']['name'])
+
+allSpeakers = dict(zip(data['speakers'][1]['name'],data['speakers'][1]['spkid']))
+
+print(allSpeakers)
+
+
+#for segments in data['segments']:
+#    #print(segments['words'])
+#        for words in segments['words']:
+#            print(words['text'])
+
+
+
   
 # Closing file
 f.close()
--- jsontest2.py
+++ jsontest2.py
@@ -1,16 +1,23 @@
 import json
+
+# declare constants
+SENTENCELEN = 250
+FPS = 25
+
 f = open('test.json',)
   
 # returns JSON object as
 # a dictionary
 data = json.load(f)
 
+# declare the dictionare allSpeakers with names and speakers
+allSpeakers = {}
+
+# populate dictionary allSpekaers with all the speakers in the transcription
 for speakers in data['speakers']:
-    print(speakers['name'])
-    print(speakers['spkid'])
-allSpeakers = {
-    "name": 
-}
+    allSpeakers[speakers['name']] = speakers['spkid']
+
+
 
 for segments in data['segments']:
     #print(segments['words'])
