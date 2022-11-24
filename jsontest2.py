ce9e37dc1e683bdc0481f8b963623bac93bbad86
Micke <micke@aplodusken.se>
2022-11-24 23:23:43 +0100

    working with formatting

--- jsontest2.py
+++ jsontest2.py
@@ -2,11 +2,14 @@ import json
 import tc
 
 
-print(tc.framestotc(100, 25))
+# print(tc.framestotc(100, 25))
 
 # declare constants
 SENTENCELEN = 250
+tcoffset = 0
 FPS = 25
+TRACK = 'V1'
+MARKER = 'White'
 
 f = open('test.json',)
   
@@ -16,18 +19,20 @@ data = json.load(f)
 
 # declare the dictionare allSpeakers with names and speakers
 allSpeakers = {}
-
+senlen = 0
+row = ''
 # populate dictionary allSpekaers with all the speakers in the transcription
 for speakers in data['speakers']:
-    allSpeakers[speakers['name']] = speakers['spkid']
-
-
+    allSpeakers[speakers['spkid']] = speakers['name']
 
 for segments in data['segments']:
-    #print(segments['words'])
-        for words in segments['words']:
-            print(words['text'])
-
+    row = allSpeakers[segments['speaker']] + "\t" + str(tc.framestotc(int(segments['words'][0]['start']) * 25, 25)) + "\t" + TRACK + "\t" + MARKER + "\t"
+    
+    for words in segments['words']:
+       
+        row = row + " " + words['text']
+       
+    print(row)
 
 
   
