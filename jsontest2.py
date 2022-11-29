aa84d7355115f75961d7957fb6622b83ce5d6a54
Micke <micke@aplodusken.se>
2022-11-29 09:20:06 +0100

    tc with offset

--- jsontest2.py
+++ jsontest2.py
@@ -1,21 +1,21 @@
+# v0.1
 import json
 import tc
 
-
+f = open('test.json',)
 # print(tc.framestotc(100, 25))
+# returns JSON object as
+# a dictionary
+data = json.load(f)
 
 # declare constants
 SENTENCELEN = 250
-tcoffset = 0
+tcoffset = data['startTimeOffset']
 FPS = 25
 TRACK = 'V1'
 MARKER = 'White'
 
-f = open('test.json',)
-  
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+print(tcoffset)
 
 # declare the dictionare allSpeakers with names and speakers
 allSpeakers = {}
@@ -26,7 +26,7 @@ for speakers in data['speakers']:
     allSpeakers[speakers['spkid']] = speakers['name']
 
 for segments in data['segments']:
-    row = allSpeakers[segments['speaker']] + "\t" + str(tc.framestotc(int(segments['words'][0]['start']) * 25, 25)) + "\t" + TRACK + "\t" + MARKER + "\t"
+    row = allSpeakers[segments['speaker']] + "\t" + str(tc.framestotc(int(segments['words'][0]['start']+tcoffset) * 25, 25)) + "\t" + TRACK + "\t" + MARKER + "\t"
     
     for words in segments['words']:
        
