772d52feb7852c8fa3361771519d4dbe38cb82c7
Aplodusken <micke@aplodusken.se>
2023-01-22 20:59:27 +0100

    nytt test i ambertest

---
+++ ambertest.py
@@ -0,0 +1,72 @@
+# v0.4
+import json
+import tc
+from pathlib import Path
+import sys
+import codecs
+
+
+def framestotc(someframes, fps):
+
+    s, f = divmod(int(someframes), fps)
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+    d, h = divmod(h, 24)
+    return '{1:02d}:{2:02d}:{3:02d}:{4:02d}'.format(d, h,m,s,f)
+
+# open the input file and name it f
+f = open(sys.argv[1],)
+
+# returns JSON object as a dictionary and store it in the data variable
+data = json.load(f)
+# Closing file f
+f.close()
+
+
+# declare constants
+# variable for future feature to add sentence length limitation 
+SENTENCELEN = 230
+
+# get the offset from the json file
+tcoffset = data['startTimeOffset']
+
+# Variable to store name of destination file
+orgdestfilename = Path(str(data['filename']))
+destfilename = orgdestfilename.with_suffix('.txt')
+# Variable to store FPS value
+FPS = 25
+# Variable to store what track to add the markers
+TRACK = 'V1'
+# Variable to store what colors we're adding to the markers
+MARKER = 'White'
+
+# open a new file for storing the result text encoding mac-roman
+f = codecs.open(destfilename, "a", "mac-roman")
+# declare the dictionare allSpeakers with names and speakers
+allSpeakers = {}
+# senlen = 0
+row = ''
+
+# populate dictionary allSpekaers with all the speakers in the transcription
+for speakers in data['speakers']:
+    allSpeakers[speakers['spkid']] = speakers['name']
+
+print(allSpeakers)
+
+for segments in data['segments']:
+
+    row = allSpeakers[segments['speaker']] + "\t" + str(framestotc(float(segments['words'][0]['start']+tcoffset) * 25, FPS)) + "\t" + TRACK + "\t" + MARKER + "\t" + "(" + allSpeakers[segments['speaker']] + ") "
+    
+    for words in segments['words']:
+        
+
+        row = row + " " + words['text']
+
+
+    f.write(row  + "\n")
+
+    
+
+f.close()
+
+print("KLAR!")
